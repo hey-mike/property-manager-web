0690805ff136e25d88a0aa41fd78126f0dd0ed15
MagicMike90 <lcw303661398@hotmail.com>
2018-05-21 22:54:30 +1000

    update

---
+++ app-client/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import './App.css';
+
+const { Header, Footer, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
