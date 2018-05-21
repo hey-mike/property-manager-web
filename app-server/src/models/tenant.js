d8b1ed6539e4eeedcde93ec0c1ac7e752843a7cf
MagicMike90 <michael.luo0801@gmail.com>
2018-05-21 16:12:27 +1000

    update

---
+++ app-server/src/models/tenant.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tenantSchema = new Schema({
+  id: Number,
+  name: {
+    type: Object,
+    required: true,
+    text: true
+  },
+  gender: String,
+  age: Number,
+  title: String,
+  email: String,
+  phone: String,
+  address: String,
+}, {
+  timestamps: true
+});
+
+
+tenantSchema.virtual('fullName').get(function () {
+  return this.name.first + ' ' + this.name.last;
+}).set(function (v) {
+  this.name.first = v.substr(0, v.indexOf(' '));
+  this.name.last = v.substr(v.indexOf(' ') + 1);
+});
+tenantSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+tenantSchema.index({
+  name: 'text'
+});
+// Configure the 'tenantSchema' to use getters and virtuals when transforming to JSON
+tenantSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
+});
+
+const tenant = mongoose.model("tenants", tenantSchema);
+
+module.exports = tenant;
