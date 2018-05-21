1bcaac517f3ef67a23aa0be4559deaa331c966bc
MagicMike90 <lcw303661398@hotmail.com>
2018-05-21 22:34:29 +1000

    add antd

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { Button } from 'antd';
+  import './App.css';
 
-export default App;
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+      );
+    }
+  }
+
+  export default App;
