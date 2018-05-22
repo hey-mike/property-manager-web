ca6ca23cba533200e517bef74b47b850e8abc416
MagicMike90 <michael.luo0801@gmail.com>
2018-05-23 08:59:37 +1000

    seeting up eslint

--- app-client/src/features/user/forms/LoginForm.jsx
+++ app-client/src/features/user/forms/LoginForm.jsx
@@ -26,21 +26,14 @@ class LoginForm extends React.Component {
           <Form onSubmit={this.handleSubmit.bind(this)} className="login-form">
             <FormItem>
               {getFieldDecorator('email', {
-                rules: [
-                  { required: true, message: 'Please input your email!' },
-                ],
+                rules: [{ required: true, message: 'Please input your email!' }]
               })(
-                <Input
-                  prefix={<Icon type="mail" style={{ fontSize: 13 }} />}
-                  placeholder="Email"
-                />
+                <Input prefix={<Icon type="mail" style={{ fontSize: 13 }} />} placeholder="Email" />
               )}
             </FormItem>
             <FormItem>
               {getFieldDecorator('password', {
-                rules: [
-                  { required: true, message: 'Please input your Password!' },
-                ],
+                rules: [{ required: true, message: 'Please input your Password!' }]
               })(
                 <Input
                   prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
@@ -52,16 +45,12 @@ class LoginForm extends React.Component {
             <FormItem>
               {getFieldDecorator('remember', {
                 valuePropName: 'checked',
-                initialValue: true,
+                initialValue: true
               })(<Checkbox>Remember me</Checkbox>)}
               <Link className="login-form-forgot" to="/forget">
                 Forgot password
               </Link>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-              >
+              <Button type="primary" htmlType="submit" className="login-form-button">
                 Log in
               </Button>
               Or <Link to="/signup">register now!</Link>
@@ -73,7 +62,7 @@ class LoginForm extends React.Component {
   }
 }
 LoginForm.prototypes = {
-  history: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 export default connect()(Form.create()(LoginForm));
