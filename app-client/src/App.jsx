a09cc54fe2d8dfcaa445b4e511bf65cd4f1de54c
MagicMike90 <lcw303661398@hotmail.com>
2018-05-31 21:50:32 +1000

    fix routers

--- app-client/src/App.jsx
+++ app-client/src/App.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { withRouter } from 'react-router-dom';
 import AuthService from './core/services/auth.service';
 import './App.css';
 
 import UserPage from './features/user/UserPage';
 import DashboardPage from './features/dashboard/DashboardPage';
-
-const PageFade = props => {
-  return (
-    <CSSTransition
-      {...props}
-      classNames="fadeTranslate"
-      timeout={1000}
-      mountOnEnter={true}
-      unmountOnExit={true}
-    />
-  );
-};
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -28,42 +15,34 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
       AuthService.isUserAuthenticated() ? (
         <Component {...props} />
       ) : (
-        <Redirect
-          exact
-          to={{
-            pathname: '/user/login',
-            state: { from: props.location },
-          }}
-        />
+        <Redirect to="/user/login" />
       )
     }
   />
 );
 
-class App extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-  };
+const NoMatch = () => <div>Can't find any route</div>;
 
+class App extends React.Component {
   render() {
     const { location } = this.props;
 
     return (
       <div className="main">
-        <TransitionGroup>
-          <PageFade key={location.pathname}>
-            <Switch location={location}>
-              <PrivateRoute path="/dashboard" component={DashboardPage} />
-              <Route path="/user" component={UserPage} />
-              <Redirect exact from="/" to="/dashboard" />
-            </Switch>
-          </PageFade>
-        </TransitionGroup>
+        <Switch location={location}>
+          <PrivateRoute path="/dashboard" component={DashboardPage} />
+          <Route path="/user" component={UserPage} />
+          <Redirect exact from="/" to="/dashboard" />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     );
   }
 }
+App.propTypes = {
+  match: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+};
 
 export default withRouter(App);
