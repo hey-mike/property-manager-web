c04caa516a3e208c0c0b759ab8785de6f4801749
MagicMike90 <lcw303661398@hotmail.com>
2018-06-01 21:25:10 +1000

    refactor

--- app-server/src/config/passport.js
+++ app-server/src/config/passport.js
@@ -3,25 +3,66 @@
 
 // Load the module dependencies
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const tokenManager = require('../utils/tokenManager');
 const User = require('../models/user');
 
-require('./strategies/local.js');
-require('./strategies/jwt.js');
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = tokenManager.publicKey;
+opts.algorithm = 'RS256';
+passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findOne(
+      {
+        id: jwt_payload.sub
+      },
+      (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        return user ? done(null, user) : done(null, false);
+      }
+    );
+  })
+);
 
-// Use Passport's 'serializeUser' method to serialize the user id
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-// Use Passport's 'deserializeUser' method to load the user document
-passport.deserializeUser(function(id, done) {
-  User.findOne(
+// // Create the Local strategy configuration method
+// module.exports = function () {
+// Use the Passport's Local strategy
+passport.use(
+  new LocalStrategy(
     {
-      _id: id
+      usernameField: 'email',
+      passwordField: 'password',
+      session: false
     },
-    '-password -salt',
-    function(err, user) {
-      done(err, user);
+    function(email, password, done) {
+      // Use the 'User' model 'findOne' method to find a user with the current username
+      User.findOne({ email: email }, (err, user) => {
+        // If an error occurs continue to the next middleware
+        if (err) {
+          return done(err);
+        }
+        // If a user was not found, continue to the next middleware with an error message
+        if (!user) {
+          return done(null, false, {
+            message: 'Unknown user'
+          });
+        }
+
+        // If the passport is incorrect, continue to the next middleware with an error message
+        if (!user.authenticate(password)) {
+          return done(null, false, {
+            message: 'Password is incorrect'
+          });
+        }
+
+        // Otherwise, continue to the next middleware with the user object
+        return done(null, user);
+      });
     }
-  );
-});
+  )
+);
