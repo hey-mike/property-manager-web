8c61a914457f0ffe8508a3d4212851619cd1c8bd
MagicMike90 <lcw303661398@hotmail.com>
2018-06-03 22:35:08 +1000

    update

--- app-client/src/features/dashboard/budgetSection/DateRangePicker.jsx
+++ app-client/src/features/dashboard/budgetSection/DateRangePicker.jsx
@@ -3,33 +3,6 @@ import { DatePicker } from 'antd';
 import moment from 'moment';
 const { RangePicker } = DatePicker;
 
-function range(start, end) {
-  const result = [];
-  for (let i = start; i < end; i++) {
-    result.push(i);
-  }
-  return result;
-}
-
-function disabledDate(current) {
-  // Can not select days before today and today
-  return current && current < moment().endOf('day');
-}
-
-function disabledRangeTime(_, type) {
-  if (type === 'start') {
-    return {
-      disabledHours: () => range(0, 60).splice(4, 20),
-      disabledMinutes: () => range(30, 60),
-      disabledSeconds: () => [55, 56],
-    };
-  }
-  return {
-    disabledHours: () => range(0, 60).splice(20, 4),
-    disabledMinutes: () => range(0, 31),
-    disabledSeconds: () => [55, 56],
-  };
-}
 class DateRangePicker extends React.Component {
   onChange(dates, dateStrings) {
     console.log('From: ', dates[0], ', to: ', dates[1]);
