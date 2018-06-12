d0b6f73da1ed191a2e36548de80bc8672fb2f892
MagicMike90 <michael.luo0801@gmail.com>
2018-06-12 17:43:19 +1000

    create client

--- app-server/src/services/emailService.js
+++ app-server/src/services/emailService.js
@@ -7,19 +7,53 @@ class EmailService {
 
   async connect() {
     try {
-      this.connection = await amqp.connect(config.get('amqp'));
-      console.log('Connect to email service queue:', config.get('amqp'));
+      this.connection = await amqp.connect(config.get('amqp:uri'));
+      console.log('Connect to email service queue:', config.get('amqp:uri'));
+
+      await this.createChannel();
+      await this.sendEmail();
     } catch (err) {
       console.trace('Email service queue connect failed: ', err);
     }
   }
   async createChannel() {
     try {
-        this.channel = await this.connection.createChannel();
-        console.log('Create channel successfully', config.get('amqp'));
-      } catch (err) {
-        console.trace('Email service queue connect failed: ', err);
+      this.channel = await this.connection.createChannel();
+      this.channel.assertQueue(config.get('amqp:queue'), { durable: false });
+      console.log('Create channel successfully');
+    } catch (err) {
+      console.trace('Create channel failed: ', err);
+    }
+  }
+
+  async publish(content) {
+    try {
+      await this.channel.sendToQueue(
+        config.get('amqp:queue'),
+        Buffer.from(JSON.stringify(content)),
+        {
+          // Store queued elements on disk
+          persistent: true,
+          contentType: 'application/json'
+        }
+      );
+    } catch (err) {
+      console.trace('Publish message failed: ', err);
+    }
+  }
+
+  async sendEmail() {
+    try {
+      for (let i = 0; i < 100; i++) {
+        await this.publish({
+          to: 'recipient@example.com',
+          subject: 'Test message #' + i,
+          text: 'hello world!'
+        });
       }
+    } catch (err) {
+      console.trace('Send email failed: ', err);
+    }
   }
 }
 
