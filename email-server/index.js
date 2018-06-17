fcba5b83b2f1d1b258a4f0ed1e60f18d739874b7
MagicMike90 <michael.luo0801@gmail.com>
2018-06-18 09:52:32 +1000

    adding email template

--- email-server/index.js
+++ email-server/index.js
@@ -1,43 +1,38 @@
 'use strict';
 const emailService =  require('./emailService');
-const nodemailer = require('nodemailer');
 
 emailService.connect();
-// emailService.consume();
 
-// // Generate test SMTP service account from ethereal.email
-// // Only needed if you don't have a real mail account for testing
-// nodemailer.createTestAccount((err, account) => {
-//   // create reusable transporter object using the default SMTP transport
-//   let transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: account.user, // generated ethereal user
-//       pass: account.pass // generated ethereal password
-//     }
-//   });
+var sender = 'smtps://youremailAddress%40gmail.com'   // The emailto use in sending the email
+//(Change the @ symbol to %40 or do a url encoding )
+var password = 'yourEmailPassword'  // password of the email to use
 
-//   // setup email data with unicode symbols
-//   let mailOptions = {
-//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//     to: 'bar@example.com, baz@example.com', // list of receivers
-//     subject: 'Hello ✔', // Subject line
-//     text: 'Hello world?', // plain text body
-//     html: '<b>Hello world?</b>' // html body
-//   };
+var nodeMailer = require("nodemailer");
+var EmailTemplate = require('email-templates').EmailTemplate;
 
-//   // send mail with defined transport object
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     console.log('Message sent: %s', info.messageId);
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+var transporter = nodeMailer.createTransport(sender + ':' + password + '@smtp.gmail.com');
 
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//   });
-// });
+// create template based sender function
+// assumes text.{ext} and html.{ext} in template/directory
+var sendResetPasswordLink = transporter.templateSender(
+  new EmailTemplate('./templates/resetPassword'), {
+    	from: 'hello@yourdomain.com',
+  });
+
+exports.sendPasswordReset = function (email, username, name, tokenUrl) {
+    // transporter.template
+    sendResetPasswordLink({
+        to: email,
+        subject: 'Password Reset - YourDomain.com'
+    }, {
+        name: name,
+        username: username,
+        token: tokenUrl
+    }, function (err, info) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Link sent\n'+ JSON.stringify(info));
+        }
+    });
+};
