2037cff698578a9ab3d818f1b6b40d7a68021b30
MagicMike90 <lcw303661398@hotmail.com>
2018-06-18 22:36:27 +1000

    add service page

--- app-client/src/features/service/ServicePage.jsx
+++ app-client/src/features/service/ServicePage.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
+import { List, Row, Col } from 'antd';
+import ServiceCard from './service-card/ServiceCard';
+
+const data = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+  {
+    title: 'Title 5',
+  },
+  {
+    title: 'Title 6',
+  },
+];
 
 class ServicePage extends React.Component {
   render() {
-    return <div>ServicePage</div>;
+    return (
+      // <Row gutter={16}>
+      //   <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+      //     <ServiceCard title="Plumber" description="this is description" />
+      //   </Col>
+      //   <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+      //     <ServiceCard title="Electrician" description="this is description" />
+      //   </Col>
+      //   <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+      //     <ServiceCard title="plumber" description="this is description" />
+      //   </Col>
+      // </Row>
+      <List
+        grid={{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 2, xxl: 3 }}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <ServiceCard title={item.title} description="this is description" />
+          </List.Item>
+        )}
+      />
+    );
   }
 }
 export default ServicePage;
---
+++ app-client/src/features/service/service-card/ServiceCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Icon, Avatar } from 'antd';
+import { withRouter } from 'react-router-dom';
+import './ServiceCard.css';
+const { Meta } = Card;
+
+class ServiceCard extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('LoginPage', props);
+  }
+
+  render() {
+    const { title, description, email, phone } = this.props;
+    return (
+      <Card actions={[<Icon type="mail" />, <Icon type="phone" />]}>
+        <Meta
+          avatar={
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          }
+          title={title}
+          description={description}
+        />
+      </Card>
+    );
+  }
+}
+ServiceCard.prototypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  email: PropTypes.string,
+  phone: PropTypes.string,
+  history: PropTypes.object.isRequired,
+};
+
+export default withRouter(ServiceCard);
