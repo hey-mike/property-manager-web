4098be00aa20e03d630e1d6145f6f797b2f683c9
MagicMike90 <lcw303661398@hotmail.com>
2018-06-19 22:50:59 +1000

    seding email successfully

--- email-server/app.js
+++ email-server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mailer = require('./nodeMailerWithTemp');
 
 var app = express();
 
@@ -14,15 +15,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
+app.get('/', function(req, res) {
+  res.send('GET request to the homepage');
+});
 
 // POST method route
-app.post('/send', function (req, res) {
+app.post('/send', async function(req, res) {
+  const { to } = req.body;
   // res.send('POST request to the homepage')
-  var mail = require('./nodeMailerWithTemp');
-  return mail.sendPasswordReset('RECEIPIENT EMAIL', 'RECIENPIENT USERNAME','RECEIPIENT NAME','http://yourdomain.com/some-password-links');
-})
+  try {
+    const result = await mailer.sendPasswordReset(
+      to,
+      'RECIENPIENT USERNAME',
+      'RECEIPIENT NAME',
+      'http://yourdomain.com/some-password-links'
+    );
+
+    res.json(result);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = app;
--- email-server/nodeMailerWithTemp.js
+++ email-server/nodeMailerWithTemp.js
@@ -1,5 +1,5 @@
-var sender = 'magicmike.test@gmail.com'; // The emailto use in sending the email(Change the @ symbol to %40 or do a url encoding )
-var password = 'Lcw20092009'; // password of the email to use
+var sender = 'TEST_EMAIL'; // The emailto use in sending the email(Change the @ symbol to %40 or do a url encoding )
+var password = 'PASS'; // password of the email to use
 
 var nodeMailer = require('nodemailer');
 var EmailTemplate = require('email-templates');
@@ -14,40 +14,51 @@ var transporter = nodeMailer.createTransport({
 
 const mailer = new EmailTemplate({
   views: { root: './templates' },
-  from: 'hello@yourdomain.com'
+  from: 'hello@yourdomain.com',
+  send: true,
+  transport: transporter
 });
 
-exports.sendPasswordReset = function(email, username, name, tokenUrl) {
-  mailer
-    .send({
-      template: 'reset-password',
-      message: {
-        to: email
-      },
-      locals: {
-        name: 'Elon'
-      }
-    })
-    .then(console.log)
-    .catch(console.error);
+exports.sendPasswordReset = async function(email, username, name, tokenUrl) {
+  return mailer.send({
+    template: 'reset-password',
+    message: {
+      to: email
+    },
+    locals: {
+      name: 'Michael'
+    }
+  });
+  // mailer
+  //   .send({
+  //     template: 'reset-password',
+  //     message: {
+  //       to: email
+  //     },
+  //     locals: {
+  //       name: 'Michael'
+  //     }
+  //   })
+  //   .then(console.log)
+  //   .catch(console.error);
 
-//   // transporter.template
-//   sendResetPasswordLink(
-//     {
-//       to: email,
-//       subject: 'Password Reset - YourDomain.com'
-//     },
-//     {
-//       name: name,
-//       username: username,
-//       token: tokenUrl
-//     },
-//     function(err, info) {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log('Link sent\n' + JSON.stringify(info));
-//       }
-//     }
-//   );
+  //   // transporter.template
+  //   sendResetPasswordLink(
+  //     {
+  //       to: email,
+  //       subject: 'Password Reset - YourDomain.com'
+  //     },
+  //     {
+  //       name: name,
+  //       username: username,
+  //       token: tokenUrl
+  //     },
+  //     function(err, info) {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         console.log('Link sent\n' + JSON.stringify(info));
+  //       }
+  //     }
+  //   );
 };
