401c4de7f60bebe4dacaed5a9d79d2cea718d051
MagicMike90 <michael.luo0801@gmail.com>
2018-06-25 17:22:12 +1000

    update

--- app-client/src/features/dashboard/budgetSection/BudgetSectionContainer.jsx
+++ app-client/src/features/dashboard/budgetSection/BudgetSectionContainer.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Tabs, Card } from 'antd';
 import IncomeGraph from './IncomeGraph';
 import ExpenseGraph from './ExpenseGraph';
-import UserActionContainer from './UserActionContainer';
+import DateRangePicker from './DateRangePicker';
 import './BudgetSectionContainer.css';
 const TabPane = Tabs.TabPane;
 
 class BudgetSectionContainer extends React.Component {
-  state = {
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
   componentDidMount() {
     setTimeout(() => {
       this.handleClick();
@@ -18,12 +23,18 @@ class BudgetSectionContainer extends React.Component {
   handleClick = () => {
     this.setState({ loading: !this.state.loading });
   };
-
+  selectDate(dates, dateStrings) {
+    console.log('From: ', dates[0], ', to: ', dates[1]);
+    console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+  }
   render() {
     return (
       <section className="dashboard-section">
         <Card loading={this.state.loading} title="Budget">
-          <Tabs tabBarExtraContent={<UserActionContainer />}>
+          <Tabs
+            tabBarExtraContent={
+              <DateRangePicker selectDate={this.selectDate} />
+            }>
             <TabPane tab="Overview" key="1" />
             <TabPane tab="Income" key="2">
               <IncomeGraph chartId="income-chart" />
--- app-client/src/features/dashboard/budgetSection/DateRangePicker.jsx
+++ app-client/src/features/dashboard/budgetSection/DateRangePicker.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import { DatePicker } from 'antd';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 const { RangePicker } = DatePicker;
 
-class DateRangePicker extends React.Component {
-  onChange(dates, dateStrings) {
-    console.log('From: ', dates[0], ', to: ', dates[1]);
-    console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
-  }
-  render() {
-    return (
-      <div>
-        <RangePicker
-          ranges={{
-            Today: [moment(), moment()],
-            'This Month': [moment(), moment().endOf('month')],
-          }}
-          onChange={this.onChange}
-        />
-      </div>
-    );
-  }
-}
+// class DateRangePicker extends React.Component {
+//   onChange(dates, dateStrings) {
+//     console.log('From: ', dates[0], ', to: ', dates[1]);
+//     console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <RangePicker
+//           ranges={{
+//             Today: [moment(), moment()],
+//             'This Month': [moment(), moment().endOf('month')],
+//           }}
+//           onChange={this.props.selectDate}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+const DateRangePicker = props => (
+  <div>
+    <RangePicker
+      ranges={{
+        Today: [moment(), moment()],
+        'This Month': [moment(), moment().endOf('month')],
+      }}
+      onChange={this.props.selectDate}
+    />
+  </div>
+);
+DateRangePicker.propTypes = {
+  selectDate: PropTypes.func,
+};
 export default DateRangePicker;
