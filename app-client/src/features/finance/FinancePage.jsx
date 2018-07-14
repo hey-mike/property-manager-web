4a8e431ebf2ee3db96732e64a4660c29e6ca19f3
MagicMike90 <lcw303661398@hotmail.com>
2018-07-14 23:29:42 +1000

    add summary

--- app-client/src/features/finance/FinancePage.jsx
+++ app-client/src/features/finance/FinancePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Row, Col } from 'antd';
 import EditIncomeTable from './EditIncomeTable';
 import EditExpenseTable from './EditExpenseTable';
+import FinanceSummary from './FinanceSummary';
 
 const EditableContext = React.createContext();
 class FinancePage extends React.Component {
@@ -11,14 +12,19 @@ class FinancePage extends React.Component {
   render() {
     return (
       <Row gutter={16}>
-        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
           <Card title="Income">
             <EditIncomeTable context={EditableContext} />
           </Card>
         </Col>
-        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
           <Card title="Expense">
             <EditExpenseTable context={EditableContext} />
+          </Card>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <Card title="Expense">
+            <FinanceSummary />
           </Card>
         </Col>
       </Row>
