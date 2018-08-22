b336612e55d618e0dd86ffb56de0a3dbf775be9d
MagicMike90 <michael.luo0801@gmail.com>
2018-08-22 14:18:17 +1000

    add more test

--- app-client/src/App.test.js
+++ app-client/src/App.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // mock local storage
 const localStorageMock = {
@@ -10,8 +16,17 @@ const localStorageMock = {
 };
 global.localStorage = localStorageMock;
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  // tree rendering
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // use shallow rendering
+  it('shallow renders without crashing', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(App)).to.have.lengthOf(1);
+  });
 });
